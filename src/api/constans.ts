15c67bc4eb6a0145e003222da55350f956828c0e
Jacek <muszynskijk@gmail.com>
2022-11-30 19:52:16 +0100

    feat(): replace API_URL env with method

--- src/api/constans.ts
+++ src/api/constans.ts
@@ -1,4 +1,6 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_PORT = 3001;
+
+export const API_URL = window.location.origin.replace("3000", "3001");
 
 export enum API_PATHS {
   TRANSACTIONS = "/transactions",
